37c7d8d59c8896c8a591efa305abd4104bce2392
Muhammad Danish Aiman <84575423+Danish-Aiman@users.noreply.github.com>
2025-07-04 05:08:35 +0800

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -19,11 +19,11 @@ resource "tls_self_signed_cert" "ssl_cert" {
 }
 
 
-# Create a Key Pair
-resource "aws_key_pair" "web_server_key" {
-  key_name   = "web-key"  # Name of the key pair
-  public_key = file("SSL/certificate/id_rsa.pub")  # Path to your public key file
+# Referencing the existing key pair
+data "aws_key_pair" "web_server_key" {
+  name   = "web-key"  # Name of the key pair
 }
+
 
 # Define the EC2 instance for the web server
 resource "aws_instance" "web_server" {
