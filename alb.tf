bc637de201789baef05252e3e95c3ec8d455dec1
Muhammad Danish Aiman <84575423+Danish-Aiman@users.noreply.github.com>
2025-07-04 01:21:33 +0800

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -9,7 +9,7 @@ resource "aws_lb" "app_alb" {
   name               = "app-alb"
   internal           = false  # Set to false for internet-facing ALB
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
+  security_groups    = [data.aws_security_group.alb_sg.id]
   subnets            = [aws_subnet.main_subnet.id]
   enable_deletion_protection = false
 
