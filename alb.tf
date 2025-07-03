82cf4b972c7b5c90bc837797e9ccfe12bf9b8988
Muhammad Danish Aiman <84575423+Danish-Aiman@users.noreply.github.com>
2025-07-04 01:33:42 +0800

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -24,28 +24,6 @@ resource "aws_lb" "app_alb" {
   }
 }
 
-# Create an ALB target group (targets are EC2 instances)
-resource "aws_lb_target_group" "app_target_group" {
-  name     = "app-target-group"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main_vpc.id
-
-  health_check {
-    path                = "/"
-    protocol            = "HTTP"
-    port                = "80"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-
-  tags = {
-    Name = "App-Target-Group"
-  }
-}
-
 # Create a listener for the Load Balancer (HTTP)
 resource "aws_lb_listener" "http_listener" {
   load_balancer_arn = aws_lb.app_alb.arn
