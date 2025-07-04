56a00bb0da90ec1dab3fec02dbe2d2ba67407dd9
Muhammad Danish Aiman <84575423+Danish-Aiman@users.noreply.github.com>
2025-07-04 16:26:29 +0800

    Add files via upload

---
+++ s3.tf
@@ -0,0 +1,25 @@
+# Create an S3 Bucket for file storage
+resource "aws_s3_bucket" "app_bucket" {
+  bucket = "web-app-file-storage-bucket"  # Choose a globally unique bucket name
+
+  tags = {
+    Name        = "AppFileStorage"
+    Environment = "Production"
+  }
+}
+
+# Create Block Public Access settings for the S3 bucket
+resource "aws_s3_bucket_public_access_block" "app_bucket_public_access" {
+  bucket = aws_s3_bucket.app_bucket.bucket
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_object" "app_file" {
+  bucket = aws_s3_bucket.app_bucket.bucket
+  key    = "private_key.pem"  # The name of the file to be uploaded
+  acl    = "private"  # Ensure the object is private
+  source = ".ssh/id_ed25519"
+}
