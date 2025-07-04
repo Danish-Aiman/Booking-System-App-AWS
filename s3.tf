5245b9d475b7568384cb0fccab3c7d7858565b80
Muhammad Danish Aiman <84575423+Danish-Aiman@users.noreply.github.com>
2025-07-04 17:02:40 +0800

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,11 +1,20 @@
+resource "random_id" "rand_id" {
+  byte_length = 8
+}
+
 # Create an S3 Bucket for file storage
-data "aws_s3_bucket" "app_bucket" {
-  bucket = "web-app-file-storage-bucket"
+resource "aws_s3_bucket" "app_bucket" {
+  bucket = "web-app-file-storage-bucket-${random_id.rand_id.hex}" 
+
+  tags = {
+    Name        = "AppFileStorage"
+    Environment = "Production"
+  }
 }
 
 # Create Block Public Access settings for the S3 bucket
 resource "aws_s3_bucket_public_access_block" "app_bucket_public_access" {
-  bucket = data.aws_s3_bucket.app_bucket.bucket
+  bucket = aws_s3_bucket.app_bucket.bucket
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
