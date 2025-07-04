a2add4998aa5d47b2d55ba6c84809091838cd638
Muhammad Danish Aiman <84575423+Danish-Aiman@users.noreply.github.com>
2025-07-04 16:32:43 +0800

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,16 +1,11 @@
 # Create an S3 Bucket for file storage
-resource "aws_s3_bucket" "app_bucket" {
-  bucket = "web-app-file-storage-bucket"  # Choose a globally unique bucket name
-
-  tags = {
-    Name        = "AppFileStorage"
-    Environment = "Production"
-  }
+data "aws_s3_bucket" "app_bucket" {
+  bucket = "web-app-file-storage-bucket"
 }
 
 # Create Block Public Access settings for the S3 bucket
 resource "aws_s3_bucket_public_access_block" "app_bucket_public_access" {
-  bucket = aws_s3_bucket.app_bucket.bucket
+  bucket = data.aws_s3_bucket.app_bucket.bucket
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
@@ -18,7 +13,7 @@ resource "aws_s3_bucket_public_access_block" "app_bucket_public_access" {
 }
 
 resource "aws_s3_object" "app_file" {
-  bucket = aws_s3_bucket.app_bucket.bucket
+  bucket = data.aws_s3_bucket.app_bucket.bucket
   key    = "private_key.pem"  # The name of the file to be uploaded
   acl    = "private"  # Ensure the object is private
   source = ".ssh/id_ed25519"
